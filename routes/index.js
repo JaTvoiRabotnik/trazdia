7d9df9e9e5ed12bbab5ba36b08c4022a19bcc7e2
Marco Zanchi <mzanchi@gmail.com>
2017-03-30 13:51:27 -0300

    express setup

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.contentType("application/pdf");
-  var process = spawn('python',["./home/marco/trazdia/collector/trazdia.py", 'Diario_Oficial_SP', '01/03/2017']);
-  process.stdout.on('data', function (data){
-    res.send(data);
+    res.status(200).send('ok');
   });
-});
 
 module.exports = router;
--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -1,77 +1,28 @@
 'use strict';
 
-var request = require('request');
-var dbSession = require('../../src/backend/dbSession.js');
-var Server = require('../../src/backend/server.js').Server;
-var resetDatabase = require('../resetDatabase.js');
-var async = require('async');
+var request = require('supertest');
+require = require('really-need');
 
-describe('The API', function () {
-
+describe('loading express', function () {
   var server;
 
-  beforeEach(function (done) {
-    server = Server('8081');
-    server.listen(function (err) {
-      resetDatabase(dbSession, function() {
-        done(err);
-      });
-    });
+  beforeEach(function () {
+    server = require('../../app', { bustCache: true });
   });
 
-  afterEach(function (done) {
-    server.close(function () {
-      resetDatabase(dbSession, function() {
-        done();
-      });
-    });
+   afterEach(function (done) {
+     server.close(done);
   });
 
 
-  it('should respond to a GET request at /api/keywords/', function (done) {
-    var expected = {
-      "_items": [
-        {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
-        {'id': 2, 'value': 'Onion', 'categoryID': 1},
-        {'id': 3, 'value': 'Knife', 'categoryID': 2}
-      ]
-    };
-    async.series(
-      [
-        function(callback) {
-          dbSession.insert(
-            'keyword',
-            {'value': 'Aubergine', 'categoryID': 1},
-            function(err) { callback(err) });
-        },
-        function(callback) {
-          dbSession.insert(
-            'keyword',
-            {'value': 'Onion', 'categoryID': 1},
-            function(err) { callback(err) });
-        },
-        function(callback) {
-          dbSession.insert(
-            'keyword',
-            {'value': 'Knife', 'categoryID': 2},
-            function(err) { callback(err) });
-        }
-      ],
-
-        function(err, results) {
-          if (err) throw(err);
-          request.get(
-            {
-              'url': 'http://localhost:8081/api/keywords/',
-              'json': true
-            },
-            function (err, res, body) {
-              expect(res.statusCode).toBe(200);
-              expect(body).toEqual(expected);
-              done();
-            }
-          );
-        }
-      );
-    });
+  it('responds to /', function testSlash(done) {
+  request(server)
+    .get('/')
+    .expect(200, done);
   });
+  it('404 everything else', function testPath(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+});

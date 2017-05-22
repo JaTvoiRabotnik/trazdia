2f2315cd03a9012bd8a62f56e185c1ec85d45aab
Marco Zanchi <mzanchi@gmail.com>
2017-05-22 12:59:59 -0300

    Firsts attempt at getting RJ journal

--- collector/views.py
+++ collector/views.py
@@ -6,6 +6,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def get_journal_id(first_level, second_level):
+    with open('journal_id.json') as data_file:
+        edition_dict = json.load(data_file)
+        return edition_dict[first_level][second_level]
+
+
 def index(request):
     return HttpResponse("TrazDia: colletor de Diarios Oficials")
 
@@ -19,6 +25,16 @@ def journal_by_date_and_section(request, journal_id, date, section):
     journal_class = locate('collector.journal.' + journal_id)
     journal = journal_class(date)
     content = journal.return_index(section)
+    response = HttpResponse(content_type='application/json')
+    response.write(content)
+    return response
+
+
+def journal_by_date(request, first_level, second_level, date):
+    journal_id = get_journal_id(first_level, second_level)
+    journal_class = locate('collector.journal.' + journal_id)
+    journal = journal_class(date)
+    content = journal.bring_edition()
     response = HttpResponse(content_type='application/json')
     response.write(content)
     return response
